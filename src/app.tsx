cb2a78c281e6dce0de06f4d9fad030791556046c
NigelMarshal <nigelmarshal@gmail.com>
2024-05-03 17:11:57 +0400

    feat(app): Test fetching audio data

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,29 @@
-async function main() {
-  while (!Spicetify?.showNotification) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  }
+let audioData = null;
 
-  // Show message on start.
-  Spicetify.showNotification("Hello!");
+async function getSpotifyAudioData(
+  delayBetweenRetries = 200,
+  retryAttempts = 5
+) {
+  while (retryAttempts > 0) {
+    try {
+      return await Spicetify.getAudioData();
+    } catch (error) {
+      retryAttempts--;
+      console.error("Failed to fetch audio data. Retrying...", error);
+      await new Promise((resolve) => setTimeout(resolve, delayBetweenRetries));
+    }
+  }
+  console.error("Data fetching failed after multiple attempts.");
+  return null;
+}
+
+async function testGetSpotifyAudioData() {
+  const audioData = await getSpotifyAudioData();
+  console.log("Retrieved Audio Data:", audioData);
+}
+
+async function main() {
+  testGetSpotifyAudioData();
 }
 
 export default main;
